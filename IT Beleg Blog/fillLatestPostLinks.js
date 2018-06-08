ed06a1acd3edd1c83b857c23276339087f0f53e0
Edwin Scharfe <edwin.scharfe@saxsys.de>
2018-06-08 14:18:15 +0200

    10 latest posts work

--- IT Beleg Blog/fillLatestPostLinks.js
+++ IT Beleg Blog/fillLatestPostLinks.js
@@ -1,9 +1,17 @@
-//TODO posts are json
 let xhttp;
 xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
+        let responseTextJson = JSON.parse(this.responseText);
+        for (let i = 0; i < 10; i++) {
 
+            let fileName = responseTextJson.post[i].file;
+            document.getElementById("recent-post-link-" + i).innerHTML = responseTextJson.post[i].title;
+            document.getElementById("recent-post-link-" + i).onclick = function () {
+                localStorage.setItem("fileName", fileName);
+            };
+            document.getElementById("recent-post-link-" + i).href = "readPost.php"
+        }
     }
 };
 xhttp.open("GET", "getLatestPosts.php", true);
--- IT Beleg Blog/getLatestPosts.php
+++ IT Beleg Blog/getLatestPosts.php
@@ -1,19 +1,34 @@
 <?php
 $files = glob("./Posts/P_*.json");
 
-$returnArray = array();
+$jsonArray = array();
+
+/**
+for ($i = 0; $i < count($files); $i++) {
+    $fileContent = fread(fopen($files[$i], "r"), filesize($files[$i]));
+    $jsonContent = json_decode($fileContent, true);
+    $jsonContent["file"] = $files[$i];
+}
+**/
 
 foreach ($files as $file){
     $fileContent = fread(fopen($file, "r"), filesize($file));
     $jsonContent = json_decode($fileContent, true);
     $jsonContent["file"] = $file;
-    array_push($returnArray, $jsonContent);
+
+    array_push($jsonArray, $jsonContent);
 }
 
-foreach ($returnArray as $key => $row) {
+foreach ($jsonArray as $key => $row) {
     $date[$key] = $row["dateOfCreation"];
 }
 
-array_multisort($date, SORT_DESC, $returnArray);
+array_multisort($date, SORT_DESC, $jsonArray);
+
+$returnArray = array();
+
+for ($i = 0; $i < count($files); $i++) {
+    $returnArray["post"][] = $jsonArray[$i];
+}
 
 echo json_encode($returnArray);
