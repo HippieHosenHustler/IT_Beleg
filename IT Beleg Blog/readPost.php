20d3b71aecdfd0ebab5ad9c8bcb914dddbc065b0
Edwin Scharfe <edwin.scharfe@saxsys.de>
2018-06-10 17:47:28 +0200

    Can delete Posts

--- IT Beleg Blog/readPost.php
+++ IT Beleg Blog/readPost.php
@@ -29,7 +29,7 @@
                     <span class="caret"></span></a>
                 <ul class="dropdown-menu">
                     <li><a href="newPost.php">New Post</a></li>
-                    <li><a href="postList.php">Edit Post</a></li>
+                    <li><a href="postList.php">Edit or Delete Post</a></li>
                     <li><a href="#">Upload Picture</a></li>
                 </ul>
             </li>
